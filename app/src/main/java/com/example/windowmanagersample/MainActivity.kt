df9f541146816d88da5aa42ad2baaafd7785abf1
cappie5551 <l.magunas@gmail.com>
2023-01-17 21:58:43 +0900

    permission

--- app/src/main/java/com/example/windowmanagersample/MainActivity.kt
+++ app/src/main/java/com/example/windowmanagersample/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.windowmanagersample
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import com.example.windowmanagersample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        // ID for the runtime permission dialog
+        private const val OVERLAY_PERMISSION_REQUEST_CODE = 1
+    }
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        // トグルボタンの設定
     }
+
+    private fun requestOverlayPermission() {
+        if (isOverlayGranted()) {
+            // オーバーレイがすでに許可されている
+            return
+        }
+
+        // オーバーレイ設定画面に遷移する
+        val intent = Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Uri.parse("package:$packageName")
+        )
+        startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (!isOverlayGranted()) {
+                // オーバーレイ設定画面から戻ってきてもオーバーレイが許可されていない場合はアプリを終了する
+                finish()
+            }
+        }
+    }
+
+    private fun isOverlayGranted() =
+        Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)
+
 }
